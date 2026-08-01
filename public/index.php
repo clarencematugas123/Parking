26c3eaec15754c487aecd6edf14d7d32ce9fdc50
Clarence Virgil R. Matugas <clarencematugas123@gmail.com>
2026-08-01 11:21:24 +0800

    final pls

--- public/index.php
+++ public/index.php
@@ -17,5 +17,4 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$response = $app->handleRequest(Request::capture());
-$response->send();
+$app->handleRequest(Request::capture());
