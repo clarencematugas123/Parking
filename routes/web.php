9b700daf50a1419dfeb9d77f0236134867cdfc7b
Clarence Virgil R. Matugas <clarencematugas123@gmail.com>
2026-08-01 10:03:27 +0800

    middle 3.0

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,9 @@ Route::inertia('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
     Route::get('dashboard', [ProductController::class, 'index'])->name('dashboard');
-    Route::post('products', [ProductController::class, 'store'])->name('parking.store');
-    Route::put('products/{product}', [ProductController::class, 'update'])->name('parking.update');
-    Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('parking.destroy');
+    Route::post('parking', [ProductController::class, 'store'])->name('parking.store');
+    Route::put('parking/{license_plate}', [ProductController::class, 'update'])->name('parking.update');
+    Route::delete('parking/{license_plate}', [ProductController::class, 'destroy'])->name('parking.destroy');
 });
 
 require __DIR__.'/settings.php';
